8e050928300e7b22d24a9de6508663850bbfdfae
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2020-06-21 03:26:50 +0700

    add listview for skill page
    add listview builder (with each element using card) for skill page.

--- resume/lib/skillPage.dart
+++ resume/lib/skillPage.dart
@@ -1,6 +1,17 @@
-	import 'package:flutter/material.dart';
- 
+import 'package:flutter/material.dart';
+
 class SkillPage extends StatelessWidget {
+  List<String> skills = [
+    'Web Development \n(ReactJS, Redux, HTML, CSS, Javascript)',
+    'Back End Development \n(Java and Go)',
+    'Linux Environment',
+    'Relational Database \n(PostgreSQL, Oracle RDBMS)',
+    'Non Relational Database \n(Redis, MongoDB)',
+    'Development Tools \n(Docker, Jenkins, Git)',
+    'Message Queues \n(NSQ, Google Pub/Sub)',
+    'Microservices \n(with REST API and gRPC)',
+    'Mobile App Development \n(using Flutter)'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +20,17 @@ class SkillPage extends StatelessWidget {
         title: new Text('Skill'),
         centerTitle: true,
       ),
+      body: ListView.builder(
+        itemCount: skills.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.laptop),
+              title: Text(skills[index], style: TextStyle(fontSize: 15.0),),
+            ),
+          );
+        },
+      )
     );
   }
 }
